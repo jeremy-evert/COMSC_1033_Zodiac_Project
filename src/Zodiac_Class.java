2ce1efe95145dd21e9f72f2d503701733552ecb5
Jeremy Evevt <jeremy.evert@swosu.edu>
2015-10-19 09:38:17 -0500

    Have the Chinese Zodiac working

--- src/Zodiac_Class.java
+++ src/Zodiac_Class.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
 public class Zodiac_Class {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Enter the year you were born: ");
+		int year = input.nextInt();
+		/* Fun code to learn about remainders no longer needed.
+		 * int year = 1980;
+		 *
+		
+		System.out.println("When I use the % button...");
+		System.out.println("I do 6 % 4, the remainder" +
+				"I get is: " + (6%4));
+		*/
+		System.out.print("\nYou were born in the year of the ");
+		
+
+		//switch (year % 12){
+		switch (year % 12){
+		case 0: System.out.println("monkey."); break;
+		case 1: System.out.println("rooster."); break;
+		case 2: System.out.println("dog."); break;
+		case 3: System.out.println("pig."); break;
+		case 4: System.out.println("rat."); break;
+		case 5: System.out.println("ox."); break;
+		case 6: System.out.println("tiger."); break;
+		case 7: System.out.println("rabbit."); break;
+		case 8: System.out.println("dragon."); break;
+		case 9: System.out.println("snake."); break;
+		case 10: System.out.println("horse."); break;
+		case 11: System.out.println("sheep."); 
+		}
 
 	}
 
